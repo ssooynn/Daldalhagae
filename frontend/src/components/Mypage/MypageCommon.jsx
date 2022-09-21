755f3e8991ea9744aca73fe9ef678ebba3b2d1d4
Yeon <theresjahn@gmail.com>
2022-09-21 13:22:47 +0900

    feat: 우편번호 검색 api 추가 및 email, phoneNo 유효성 검사 함수 추가

--- frontend/src/components/Mypage/MypageCommon.jsx
+++ frontend/src/components/Mypage/MypageCommon.jsx
@@ -42,7 +42,6 @@ export const FlexBox= ({ children, width, height, justify, direction, align, mar
 const InfoBox = styled.div`
     padding: ${(props)=> props.padding || '5px'};
     width: ${(props) => props.width || '100%'};
-    min-width: ${(props) => props.minWidth || '200px'};
     height: ${(props) => props.height || "auto"};
     font-size: ${(props)=> props.mainFontSize || '14px'};
     margin: ${(props)=> props.margin || '1px'};
@@ -55,17 +54,17 @@ const InfoBox = styled.div`
 const StyledInput = styled.input`
     padding: ${(props)=> props.padding || '5px'};
     width: ${(props) => props.width || '100%'};
-    min-width: ${(props) => props.minWidth || '200px'};
     height: ${(props) => props.height || "auto"};
     font-size: ${(props)=> props.mainFontSize || '14px'};
     margin: ${(props)=> props.margin || '1px'};
     border: 0.1px solid #969696;
     border-radius: 5px;
     box-sizing: border-box;
+    grid-column: ${(props) => props.span || "span 1"};
 
 `
 
-const SmallText = styled.div`
+export const SmallText = styled.div`
     width: auto;
     text-align: start;
     font-size: ${(props)=> props.fontSize ||'12px'};
@@ -73,15 +72,50 @@ const SmallText = styled.div`
     margin: ${(props)=> props.textMargin || '1px'};
     `
 
-export const InfoLayout = ({padding, subpadding, width, height, mainFontSize, fontSize, fontWeight, textMargin, margin, label, minWidth, span, sub, children}) => {
+export const InfoLayout = ({padding, subpadding, width, height, mainFontSize, fontSize, fontWeight, textMargin, margin, label, span, sub, children}) => {
     return(
         <FlexBox justify='start' align='start' direction='column' span={span}>
             <SmallText fontSize={fontSize} fontWeight={fontWeight} textMargin={textMargin}>{label}</SmallText>
-            <InfoBox width={width} height={height} mainFontSize={mainFontSize} margin={margin} minWidth={minWidth} padding={padding} style={{display:'flex', justifyContent:'space-between'}}>
+            <InfoBox width={width} height={height} mainFontSize={mainFontSize} margin={margin} padding={padding} style={{display:'flex', justifyContent:'space-between'}}>
                 <div style={{padding:subpadding}}>{children}</div>
                 {sub? <div style={{padding:subpadding, width:'10%', borderLeft:'0.1px solid #969696', textAlign:'center'}}>{sub}</div>:<></>}
                 </InfoBox>
 
         </FlexBox>
+    )
+}
+
+export const StyledInputBox = ({disabled, value, name, onChange, span}) => {
+    return(
+        <StyledInput disabled={disabled} value={value} name={name} onChange={onChange} span={span}></StyledInput>
+    )
+}
+
+export const InputLayout = ({padding, width, height, mainFontSize, fontSize, fontWeight, textMargin, margin, label, name, span, value, onChange}) => {
+    return(
+        <FlexBox justify='start' align='start' direction='column' span={span}>
+            <SmallText fontSize={fontSize} fontWeight={fontWeight} textMargin={textMargin}>{label}</SmallText>
+            <StyledInput onChange={onChange} value={value} name={name} width={width} height={height} mainFontSize={mainFontSize} margin={margin} padding={padding} style={{display:'flex', justifyContent:'space-between'}}></StyledInput>
+        </FlexBox>
+    )
+}
+
+const ButtonStyle = styled(animated.button)`
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.2);
+    box-sizing: border-box;
+    border: none;
+    width: 100%;
+    height: 100%;
+    border-radius: 5px;
+    background-color: #F9F5EA;
+    &:hover{
+        background-color: #776B62;
+        color:white;
+      }
+`
+
+export const AddressButton = ({children, onClick}) => {
+    return (
+        <ButtonStyle onClick={onClick}>{children}</ButtonStyle>
     )
 }
