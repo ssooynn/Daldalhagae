167fc492b38f9d790a6fa470ffd02d0f53ea1114
이경무 <app928@naver.com>
2022-09-23 15:24:15 +0900

    feat: review API

---
+++ BE/Server/business/src/main/java/com/ssafy/a302/repository/SubscribtionHistoryRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.a302.repository;
+
+import com.ssafy.a302.domain.SubscribtionHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribtionHistoryRepository extends JpaRepository<SubscribtionHistory,Integer> {
+
+}
