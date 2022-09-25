cad1066aebf62cd6fe5d2a7d1e2ab4c0672c757b
이경무 <app928@naver.com>
2022-09-26 08:57:04 +0900

    ãreview API complete

---
+++ BE/Server/business/src/main/java/com/ssafy/a302/repository/ServiceReviewRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.a302.repository;
+
+import com.ssafy.a302.domain.ServiceReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceReviewRepository extends JpaRepository<ServiceReview,Integer> {
+}
