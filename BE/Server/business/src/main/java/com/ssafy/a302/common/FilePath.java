ac52edbf91da341d15bce1cda0e1f7bfb044c8c4
이준경 <away0419@naver.com>
2022-09-26 01:00:00 +0900

    feat : 파일용 서버 추가, 파일 경로 설정

---
+++ BE/Server/business/src/main/java/com/ssafy/a302/common/FilePath.java
@@ -0,0 +1,17 @@
+package com.ssafy.a302.common;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@ConfigurationProperties("custom")
+@Data
+public class FilePath {
+	private String petImageLoadPath;
+	private String productImageLoadPath;
+	private String petImageUploadPath;
+	private String productImageUploadPath;
+	
+}
